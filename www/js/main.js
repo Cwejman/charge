d89e82ba2b268b252eafb2a4f199ad7ad7293460
Cwejman <jcwejman@gmail.com>
2016-02-24 19:59:17 +0100

    Cached DOM elements

--- www/js/main.js
+++ www/js/main.js
@@ -6,28 +6,25 @@ requirejs.config({
 })
 
 require(['app/controller', 'utils/click'], function (Ctrl, Click) {
-
+  
+  var displayText = document.getElementById('text')
+  var leftBtn = document.getElementById('left')
+  var rigthBtn = document.getElementById('right')
+  
   setTimeout(function() {
     if (window.StatusBar) {
       StatusBar.hide()
     }
   }, 300)
 
+  displayText.innerHTML = Ctrl.init()
 
-  document.getElementById('text').innerHTML = Ctrl.init()
+  Click(leftBtn, function () {
+    displayText.innerHTML = Ctrl.swap(-1)
+  })
 
-  Click(
-    document.getElementById('left'),
-    function () {
-      document.getElementById('text').innerHTML = Ctrl.swap(-1)
-    }
-  )
-
-  Click(
-    document.getElementById('right'),
-    function () {
-      document.getElementById('text').innerHTML = Ctrl.swap(1)
-    }
-  )
+  Click(rigthBtn, function () {
+    displayText.innerHTML = Ctrl.swap(1)
+  })
 
 })
