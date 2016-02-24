b1f428319822a9c12316833a3a094113bd6be29a
Cwejman <jcwejman@gmail.com>
2016-02-24 19:59:50 +0100

    Remove hide-statusbar attempt

--- www/js/main.js
+++ www/js/main.js
@@ -10,12 +10,6 @@ require(['app/controller', 'utils/click'], function (Ctrl, Click) {
   var displayText = document.getElementById('text')
   var leftBtn = document.getElementById('left')
   var rigthBtn = document.getElementById('right')
-  
-  setTimeout(function() {
-    if (window.StatusBar) {
-      StatusBar.hide()
-    }
-  }, 300)
 
   displayText.innerHTML = Ctrl.init()
 
